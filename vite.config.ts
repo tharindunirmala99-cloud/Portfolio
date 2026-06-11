3ec3df3a0d3c9014649bd60114272416b5b9ebed
tharindug99 <tharindunirmala99@gmail.com>
2026-06-11 09:22:17 +0530

    chore: update package.json and vite.config.ts for improved build process and add tsconfig.tsbuildinfo

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
-  base: '/Portfolio/',
-
+  base: '/Portfolio/',           // Important for GitHub Pages
   plugins: [react(), tailwindcss()],
-
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '.'),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
 });
